f5cb8fb2c9dfa214b0a9c2efaaf1178e16b279d2
Marika Allely and Mollie Bootsma <student@epicodus.com>
2016-02-10 10:31:06 -0800

    add more properties

--- src/Places.php
+++ src/Places.php
@@ -2,11 +2,15 @@
 class Place
 {
     private $city;
-    
+    private $year;
+    private $picture;
 
-    function __constructor($city)
+
+    function __construct($city, $year, $picture)
     {
         $this->city = $city;
+        $this->year = $year;
+        $this->picture = $picture;
     }
 
     function setCity($city_name)
@@ -17,6 +21,41 @@ class Place
     function getCity()
     {
         return $this->city;
+    }
+
+    function setYear($year)
+    {
+        $this->year = $year;
+    }
+
+    function getYear()
+    {
+        return $this->year;
+    }
+
+    function setPicture($picture)
+    {
+        $this->picture = $picture;
+    }
+
+    function getPicture()
+    {
+        return $this->picture;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_places'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_places'];
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['list_of_places'] = array();
     }
 }
 
